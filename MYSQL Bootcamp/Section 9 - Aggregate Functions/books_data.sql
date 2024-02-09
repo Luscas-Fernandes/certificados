549e285de601e211a518febcaa3f17d0a419ae7a
Lucas Fernandes <lfaa2057@gmail.com>
2024-02-09 15:04:34 -0300

    Update books_data.sql

--- MYSQL Bootcamp/Section 9 - Aggregate Functions/books_data.sql
+++ MYSQL Bootcamp/Section 9 - Aggregate Functions/books_data.sql
@@ -15,9 +15,35 @@ SELECT COUNT(*) FROM books WHERE title LIKE '%the%'; -- Count the rows where the
 --
 
 -- GROUP BY
+
+SELECT COUNT(*) FROM books;
+
 SELECT author_lname, COUNT(*) AS books_written FROM books GROUP BY author_lname ORDER BY books_written DESC;
 
 SELECT released_year FROM books;
 
 SELECT released_year, COUNT(*) FROM books GROUP BY released_year ORDER BY released_year DESC;
 SELECT released_year, COUNT(*) FROM books GROUP BY released_year ORDER BY COUNT(*) DESC;
+
+-- MIN and MAX
+
+SELECT MIN(released_year) FROM books;
+SELECT MAX(pages) FROM books;
+
+SELECT MIN(author_lname), MAX(author_lname) FROM books;
+
+-- 
+
+-- Subqueries
+
+SELECT title, pages FROM books WHERE pages = (SELECT MAX(pages) FROM books); -- in this case, WHERE pages = 634, if there's another book with 634, it'll show both
+
+SELECT title, released_year FROM books WHERE released_year = (SELECT MIN(released_year) FROM books);
+
+--
+
+-- Group By, multiple columns
+
+SELECT author_fname, author_lname, COUNT(*) FROM books GROUP BY author_lname, author_fname;
+
+SELECT CONCAT(author_fname, ' ', author_lname) AS author, COUNT(*) FROM books, GROUP BY author
